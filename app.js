1913fead354664e7c4e00b0020ace85b4838e59e
Christopher Jaeger <cjaeger.ld@gmail.com>
2021-01-29 16:17:07 -0500

    Removed users require

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var { sequelize } = require('./models');
 })();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -33,7 +32,6 @@ app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
